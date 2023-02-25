b299fb286b68c0e8dbc1d444ed8b5bc166f5161a
Andy Hua <user.andy.hua@gmail.com>
2023-02-24 16:08:08 -0800

    adding user views

--- users/models.py
+++ users/models.py
@@ -7,7 +7,8 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, null=True, blank=True)
-    bio = models.TextField()
+    image = models.ImageField(blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return str(self.user)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+
+class RegisterView():
+    pass
+
+
+# class LoginView(APIView):
+#     def post(self, request):
+#         username = request.data['username']
+#         password = request.data['password']
+#         # use a method to get access token (from the package you are using)
+#         # access token class will return access token if the user is authenticated
+#         # otherwise it will return error response
+#         pass
+
+
+class LogoutView():
+    pass
